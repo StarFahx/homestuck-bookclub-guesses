5fa99f02f52e4e729cd91ea718ef012489dd5c1e
StarFahx <starfahx@gmail.com>
2020-08-16 07:54:18 +0100

    Updates

---
+++ precache-manifest.60e8a73ab2ff11cb47e4bddb96d13bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf5eb8a7a80383c0fc032fc1668ce31",
+    "url": "/homestuck-bookclub-guesses/index.html"
+  },
+  {
+    "revision": "cbd88b0fb79079f7dead",
+    "url": "/homestuck-bookclub-guesses/static/css/main.830c9fdf.chunk.css"
+  },
+  {
+    "revision": "6f424e291e013036807c",
+    "url": "/homestuck-bookclub-guesses/static/js/2.4e77cc23.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homestuck-bookclub-guesses/static/js/2.4e77cc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd88b0fb79079f7dead",
+    "url": "/homestuck-bookclub-guesses/static/js/main.15ce8414.chunk.js"
+  },
+  {
+    "revision": "85fda248c1e0db5a7ba8",
+    "url": "/homestuck-bookclub-guesses/static/js/runtime-main.8b7ce1ac.js"
+  }
+]);
